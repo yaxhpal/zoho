7cf473393f459a9891a2c03862895679d3a4e38c
Yashpal Meena <yashpal.meena@screen-magic.com>
2023-01-02 21:55:00 +0530

    [Yashpal] Minor update

--- widgets/biginnew/SMSMagicForBigin/app/js/custom.js
+++ widgets/biginnew/SMSMagicForBigin/app/js/custom.js
@@ -1,41 +1,60 @@
 $(document).ready(function () {
+  console.log("Document is ready");
+  /*
+   * Subscribe to the EmbeddedApp onPageLoad event before
+   * initializing the widget
+   */
+  ZOHO.embeddedApp.on("PageLoad", function (data) {
+    console.log("PageLoad is complete");
     /*
-    * Subscribe to the EmbeddedApp onPageLoad event before
-    * initializing the widget
+     * Verify if EntityInformation is Passed
+     */
+    if (data && data.Entity) {
+      /*
+       * Fetch Information of Record passed in PageLoad
+       * and insert the response into the dom
+       */
+      ZOHO.CRM.API.getRecord({
+        Entity: data.Entity,
+        RecordID: data.EntityId,
+      }).then(function (response) {
+        console.log("ZOHO.CRM.API.getRecord: " + response);
+        $("#recordInfo").innerHTML = JSON.stringify(response, null, 2);
+      });
+    }
+
+   /*
+    * Fetch Current User Information from CRM
+    * and insert the response into the dom
     */
-    ZOHO.embeddedApp.on("PageLoad",function(data) {
-        /*
-        * Verify if EntityInformation is Passed
-        */
-        if(data && data.Entity) {
-            /*
-            * Fetch Information of Record passed in PageLoad
-            * and insert the response into the dom
-            */
-            ZOHO.CRM.API.getRecord({Entity:data.Entity,RecordID:data.EntityId}).then(function(response) {
-                $("#recordInfo").innerHTML = JSON.stringify(response, null, 2);
-            });
-        }
-        /*
-        * Fetch Current User Information from CRM
-        * and insert the response into the dom
-        */
-        ZOHO.CRM.CONFIG.getCurrentUser().then(function(response) {
-            $("#userInfo").innerHTML = JSON.stringify(response, null, 2);
-        });
-
-        ZOHO.CRM.CONFIG.getOrgInfo().then(function(response) {
-            $("#orgInfo").innerHTML = JSON.stringify(response, null, 2);
-        });
-
-        var data_keys = {apiKeys:["smsmagic4bigin__SMS_Magic_Data_Center_URL"]};
-        ZOHO.CRM.API.getOrgVariable(data_keys).then(function (data) {
-            console.log("smsmagic4bigin__SMS_Magic_Data_Center_URL:: " + JSON.stringify(data));
-        });
-
+    ZOHO.CRM.CONFIG.getCurrentUser().then(function (response) {
+      console.log("ZOHO.CRM.CONFIG.getCurrentUser: " + response);
+      $("#userInfo").innerHTML = JSON.stringify(response, null, 2);
     });
+
     /*
-    * initialize the widget.
-    */
-    ZOHO.embeddedApp.init();
+     * Fetch Current User Information from CRM
+     * and insert the response into the dom
+     */
+    ZOHO.CRM.CONFIG.getCurrentUser().then(function (response) {
+      console.log("ZOHO.CRM.CONFIG.getCurrentUser: " + response);
+      $("#userInfo").innerHTML = JSON.stringify(response, null, 2);
+    });
+
+    ZOHO.CRM.CONFIG.getOrgInfo().then(function (response) {
+      console.log("ZOHO.CRM.CONFIG.getOrgInfo: " + response);
+      $("#orgInfo").innerHTML = JSON.stringify(response, null, 2);
+    });
+
+    // var data_keys = { apiKeys: ["smsmagic4bigin__SMS_Magic_Data_Center_URL"] };
+    // ZOHO.CRM.API.getOrgVariable(data_keys).then(function (data) {
+    //   console.log("ZOHO.CRM.API.getOrgVariable: " + date);
+    //   try {
+    //     console.log("smsmagic4bigin__SMS_Magic_Data_Center_URL:: " + JSON.stringify(data));
+    //   } catch (err) {
+    //     console.log('getOrgVariable error: ' + err);
+    //   }
+    // });
+  });
+  ZOHO.embeddedApp.init();
 });

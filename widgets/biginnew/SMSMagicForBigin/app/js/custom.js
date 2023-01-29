372c9c6bf1223003f8ec4c399307f4a4773ff1b2
Yashpal Meena <yashpal.meena@screen-magic.com>
2023-01-30 01:05:15 +0530

    [Yashpal] Updates 30 Jan 2023 with stub

--- widgets/biginnew/SMSMagicForBigin/app/js/custom.js
+++ widgets/biginnew/SMSMagicForBigin/app/js/custom.js
@@ -1,169 +1,156 @@
 var magicToast = null;
-var magicInputToast = null;
+
 var baseURLAPIName = "smsmagic4bigin__Base_URL";
+var BASE_URL_INPUT_ID = "#baseURL";
+
+var PHONE_FIELDS_CONTAINER_ID = "#phone-fields";
 var phoneFieldAPIName = "smsmagic4bigin__Phone_Field";
-var accountDetailsAPIName = "smsmagic4bigin__Account_Details";
-var orgInfo = {};
+var PHONE_FIELD_INPUT_ID = "#inputPhoneField";
 
+var IS_FIELD_TOUCHED = false;
+
+var SAVE_SETTINGS_BTN_ID = "#save-settings";
+var HTTP_SUCCESS_CODE = "200";
+var SMS_MAGIC_ACCOUNT_API_PATH = "/api/v2/bigin/account"
+
+var account_syncing = false;
+
+// Save Bigin variable
 function saveOrgVar(apiName, apiValue) {
-  let params = { apiname: apiName, value: apiValue };
-  ZOHO.BIGIN.API.setBiginVariable(params).then(function (data) {
-    console.log(data);
-    response = JSON.parse(data);
-    if (response.status_code === "200") {
-      $("#inputdcurl").val(apiValue);
-      magicToast.show();
-    }
+  ZOHO.BIGIN.API.setBiginVariable({apiname: apiName, value: apiValue}).then(function (response) {
+    console.log("saveOrgVar: " + JSON.stringify(response, null, 2));
   });
 }
 
-function selectPhoneField(phonleFieldAPI, phoneFieldLabel) {
-  let params = {apiname: phoneFieldAPIName, value: phonleFieldAPI};
-  ZOHO.BIGIN.API.setBiginVariable(params).then(function (data) {
-    console.log(data);
-    response = JSON.parse(data);
-    if (response.status_code === "200") {
-      $("#inputPhoneField").val(phoneFieldLabel);
-    }
-  });
-}
-
-function selectDataCenter(dc) {
-  console.log("Selected DC: " + dc);
-  $("#usdc").removeClass("disabled");
-  $("#eudc").removeClass("disabled");
-  $("#audc").removeClass("disabled");
-  $("#otherdc").removeClass("disabled");
-  if (dc == 1) {
-    $("#usdc").addClass("disabled");
-    saveOrgVar(baseURLAPIName, "https://app.sms-magic.com");
-    $("#selected-dc").html("US data center");
-  } else if (dc == 2) {
-    $("#eudc").addClass("disabled");
-    saveOrgVar(baseURLAPIName, "https://eu.app.sms-magic.com");
-    $("#selected-dc").html("EU data center");
-  } else if (dc == 3) {
-    $("#audc").addClass("disabled");
-    saveOrgVar(baseURLAPIName, "https://aus-app.sms-magic.com");
-    $("#selected-dc").html("AU data center");
-  } else {
-    magicInputToast.show();
+// Set Input field value
+function setInputFieldValue(inputFieldId, inputFieldValue, inputFieldLabel) {
+  if ($(inputFieldId).val() === inputFieldValue) {
+    console.warn("No change detected");
+    return;
   }
-}
-
-  /*
-  * Fetch Current User and organization information from Bigin
-  */
-function syncAccountDetails() {
-  ZOHO.BIGIN.CONFIG.getCurrentUser().then(function (response) {
-    console.log("ZOHO.BIGIN.CONFIG.getCurrentUser: " + JSON.stringify(response, null, 2));
-    orgInfo["zoho_user_id"] = response["users"][0]["id"];
-    orgInfo["zoho_user_email"] = response["users"][0]["email"];
-    ZOHO.BIGIN.CONFIG.getOrgInfo().then(function (response) {
-      console.log("ZOHO.BIGIN.CONFIG.getOrgInfo: " + JSON.stringify(response, null, 2));
-      orgInfo["zoho_org_id"] = response["org"][0]["id"];
-      let request = {
-          url: "https://eool2p37oq9j5v9.m.pipedream.net",
-          headers: {"Content-Type": "application/json"},
-          body: orgInfo
-      }
-      ZOHO.BIGIN.HTTP.post(request).then(function (data) {
-        console.log("Request response ==> " + data);
-        data = JSON.parse(data);
-        let accountid = data.Account;
-        let apiKey = data.APIKey;
-        console.log("Request response Account ==> " + accountid + " API Key ==> " + apiKey);
-        if (apiKey != undefined && apiKey.trim() !== "" && apiKey != null) {
-          console.log("Request response ==> success");
-          $("#accountid").val(accountid);
-          $("#apikey").val(apiKey);
-          let params = { apiname: accountDetailsAPIName, value: data };
-          ZOHO.BIGIN.API.setBiginVariable(params).then(function (response) {
-              console.log("ZOHO.BIGIN.API.setBiginVariable reponse => " + response);
-          });
-        } else {
-          $("#accountstatus").removeClass("text-success");
-          $("#accountstatus").addClass("text-warning");
-          $("#accountstatus").html("&#xe002;").trigger("change");
-        }
-      });
-    });text-right
-  });
+  $(inputFieldId).val(inputFieldValue);
+  $(inputFieldId).focus();
+  if (inputFieldLabel) {
+    $(`${inputFieldId}Display`).val(inputFieldLabel);
+  }
+  IS_FIELD_TOUCHED = true;
+  $(SAVE_SETTINGS_BTN_ID).removeAttr('disabled');
 }
 
 $(document).ready(function () {
-  $("#otherdcsavebtn").click(function () {
-    let otherDCURL = $("#otherdcinputurl").val();
-    if (otherDCURL != undefined && otherDCURL.trim() !== "" && otherDCURL != null) {
-      saveOrgVar(baseURLAPIName, otherDCURL);
-      $("#selected-dc").html("Custom data center");
-    }
-  });
   /*
-   * Subscribe to the EmbeddedApp onPageLoad event before
-   * initializing the widget
+   * Subscribe to the EmbeddedApp onPageLoad event before initializing the widget
    */
   ZOHO.embeddedApp.on("PageLoad", function (data) {
     console.log("PageLoad is complete" + JSON.stringify(data, null, 2));
+
     // Get all the field names for this module
     ZOHO.BIGIN.META.getFields({ Entity: "Contacts" }).then(function (data) {
-      $("#phone-fields").val(null).trigger("change");
+      $(PHONE_FIELDS_CONTAINER_ID).val(null).trigger("change");
       data.fields.forEach(function (field) {
         if (field.data_type === "phone") {
           console.log("Adding phone field==> " + field.field_label);
-          let newOption = $("<li onclick=\"javascript:selectPhoneField('"+field.api_name+"', '"+field.field_label+"');\"><button class=\"dropdown-item\">"+field.field_label+"</a></li>");
-          $("#phone-fields").append(newOption).trigger("change");
+          let newOption = $(`<li onclick="javascript:setInputFieldValue('#inputPhoneField', '${field.api_name}', '${field.field_label}');"><button class="dropdown-item">${field.field_label}</a></li>`);
+          $(PHONE_FIELDS_CONTAINER_ID).append(newOption).trigger("change");
         }
       });
     });
 
+    // Get Base URL variable value
     ZOHO.BIGIN.API.getBiginVariable({ nameSpace: baseURLAPIName }).then(function (data) {
       console.log("ZOHO.BIGIN.API.getOrgVariable: " + JSON.stringify(data, null, 2));
-      appURL = data.Success.Content;
-      try {
-        if (appURL != null) {
-          $("#usdc").removeClass("disabled");
-          $("#eudc").removeClass("disabled");
-          $("#audc").removeClass("disabled");
-          $("#otherdc").removeClass("disabled");
-          if (appURL === "https://app.sms-magic.com") {
-            $("#usdc").addClass("disabled");
-          } else if (appURL === "https://eu.app.sms-magic.com") {
-            $("#eudc").addClass("disabled");
-          } else if (appURL === "https://aus-app.sms-magic.com") {
-            $("#audc").addClass("disabled");
-          } else {
-            $("#otherdc").addClass("disabled");
-          }
-          $("#inputdcurl").val(appURL);
-        }
-      } catch (err) {
-        console.error("Error in getting org valiable" + err);
-      }
+      $(BASE_URL_INPUT_ID).val(data.Success.Content);
     });
 
+    // Get Phone Field variable value
     ZOHO.BIGIN.API.getBiginVariable({ nameSpace: phoneFieldAPIName }).then(function (data) {
       console.log("ZOHO.BIGIN.API.getOrgVariable: " + JSON.stringify(data, null, 2));
-      phoneFieldLabel = data.Success.Content;
-      $("#inputPhoneField").val(phoneFieldLabel.replace("_", " "));
-    });
-
-    ZOHO.BIGIN.API.getBiginVariable({ nameSpace: accountDetailsAPIName}).then(function (data) {
-      console.log("ZOHO.BIGIN.API.getOrgVariable: " + JSON.stringify(data, null, 2));
-      accountDetails = JSON.parse(data.Success.Content);
-      let accountid = accountDetails.Account;
-      let apiKey = accountDetails.APIKey;
-      console.log("Request response Account ==> " + accountid + " API Key ==> " + apiKey);
-      if (apiKey != undefined && apiKey.trim() !== "" && apiKey != null) {
-        console.log("Request response ==> success");
-        $("#accountid").val(accountid);
-        $("#apikey").val(apiKey);
-      } else {
-        $("#accountstatus").removeClass("text-success");
-        $("#accountstatus").addClass("text-warning");
-        $("#accountstatus").html("&#xe002;").trigger("change");
-      }
+      $(`${PHONE_FIELD_INPUT_ID}Display`).val(data.Success.Content.replace("_", " "));
+      $(PHONE_FIELD_INPUT_ID).val(data.Success.Content);
     });
   });
   ZOHO.embeddedApp.init();
+
+  // Save the configured settings
+  $(SAVE_SETTINGS_BTN_ID).click(function () {
+    if (IS_FIELD_TOUCHED) {
+      $(SAVE_SETTINGS_BTN_ID).prop("disabled", true);
+      IS_FIELD_TOUCHED = false;
+      let baseURL = $(BASE_URL_INPUT_ID).val();
+      let phoneField = $(PHONE_FIELD_INPUT_ID).val();
+      console.warn(`Base URL = ${baseURL}, Phone field = ${phoneField}`);
+      if (baseURL.trim() != "") {
+        saveOrgVar(baseURLAPIName, baseURL);
+        account_syncing = false;
+      }
+      if (phoneField.trim() != "") {
+        saveOrgVar(phoneFieldAPIName, phoneField);
+      }
+      magicToast.show();
+    }
+  });
+
+  $("#signup-btn").click(function () {
+    let baseURL = $(BASE_URL_INPUT_ID).val();
+    window.open(`${baseURL}/trial`, "_blank");
+    account_syncing = false;
+  });
+
+  // Fetch Current User and organization information from Bigin
+  $("#nav-home-tab").click(function () {
+    if (account_syncing) {
+      return;
+    }
+    account_syncing = true;
+    let baseURL = $(BASE_URL_INPUT_ID).val();
+    let orgInfo = {};
+    $("#account-failed-alert").addClass("visually-hidden");
+    $("#account-success-alert").addClass("visually-hidden");
+    $("#accountstatus-loading").removeClass("visually-hidden");
+    $("#signup-btn").addClass("invisible")
+    if (baseURL.trim() != "") {
+      ZOHO.BIGIN.CONFIG.getCurrentUser().then(function (response) {
+        console.log("ZOHO.BIGIN.CONFIG.getCurrentUser: " + JSON.stringify(response, null, 2));
+        orgInfo["zoho_user_id"] = response["users"][0]["id"];
+        orgInfo["zoho_user_email"] = response["users"][0]["email"];
+        ZOHO.BIGIN.CONFIG.getOrgInfo().then(function (response) {
+          console.log("ZOHO.BIGIN.CONFIG.getOrgInfo: " + JSON.stringify(response, null, 2));
+          orgInfo["zoho_org_id"] = response["org"][0]["id"];
+          orgInfo["product"] = "bigin";
+          let request = {
+              url: `${baseURL}${SMS_MAGIC_ACCOUNT_API_PATH}`,
+              headers: {"Content-Type": "application/json"},
+              body: orgInfo
+          }
+          ZOHO.BIGIN.HTTP.post(request).then(function (data) {
+            console.log("Request response ==> " + data);
+            let accountSetup = false;
+            try {
+              data = JSON.parse(data);
+              let accountid = data.Account;
+              let apiKey = data.APIKey;
+              console.log("Request response Account ==> " + accountid + " API Key ==> " + apiKey);
+              if (apiKey != undefined && apiKey.trim() !== "" && apiKey != null) {
+                console.log("Request response ==> success");
+                $("#accountid").val(accountid);
+                $("#apikey").val(apiKey);
+                $("#account-success-alert").removeClass("visually-hidden");
+                accountSetup = true;
+              }
+            } catch (error) {
+                console.warn("syncAccountDetails: " + error);
+            }
+            if (!accountSetup) {
+              $("#account-failed-alert").removeClass("visually-hidden");
+              $("#signup-btn").removeClass("invisible")
+            }
+            $("#accountstatus-loading").addClass("visually-hidden");
+          });
+        });
+      });
+    } else {
+      $("#toast-text").html('Please configure Base URL first');
+      magicToast.show();
+    }
+  });
 });
